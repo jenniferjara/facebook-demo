d2243acbc52e6e66ad232d034aeccaf7a97fbc54
jenniferjara <lisbet.jara@gmail.com>
2016-11-01 17:28:17 -0500

    agregando fumcion para publicacion nueva

--- js/app.js
+++ js/app.js
@@ -1,17 +1,41 @@
 window.addEventListener("load", cargarPagina);
 
 function cargarPagina(){
+	var foto = document.getElementById("perfil").src;
+	localStorage.setItem("imagen", foto);
+	var textArea =  document.getElementById("textarea");
 	var boton = document.getElementById("btn");
 	boton.addEventListener("click", crearMensaje);
 }
 
 function crearMensaje(e){
 	e.preventDefault();
-	var contenedor = document.getElementById("mensaje");
-	var textArea =  document.getElementById("textarea");
-	var mensaje = document.createElement("div");
-	mensaje.textContent = textArea.value;
+	var contenedor = document.getElementById("mensajes");
+	var texto = this.previousElementSibling.previousElementSibling;
+	console.log(texto);
+	var cajaMensaje = document.createElement("div");
+	cajaMensaje.classList.add("p-3");
 
-	contenedor.appendChild(mensaje);
-	textArea.value="";
+	var rowUno = document.createElement("div");
+	rowUno.className = "row";
+	var colUno = document.createElement("div");
+	var colDos = document.createElement("div");
+	colUno.className = "col s2";
+	colDos.className = "col s10";
+	var fotoPerfil = document.createElement("img");
+	var src = localStorage.getItem("imagen");
+	fotoPerfil.setAttribute("src", src);
+	fotoPerfil.classList.add("circle", "responsive-img");
+	colUno.appendChild(fotoPerfil);
+	rowUno.appendChild(colUno);
+	var mensaje = document.createElement("div");
+	var fotoNombre =document.createElement("div");
+
+	mensaje.textContent = texto.value;
+
+	cajaMensaje.appendChild(rowUno);
+	cajaMensaje.appendChild(mensaje);
+	contenedor.appendChild(cajaMensaje);
+
+	texto.value="";
 }
